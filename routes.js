6f27572be058084dced1986a5eea1e1dd6d86399
Rishi Raj Jain <46300090+rishi-raj-jain@users.noreply.github.com>
2022-06-24 21:22:12 +0530

    Add caution message

--- routes.js
+++ routes.js
@@ -3,3 +3,14 @@ import { Router } from '@layer0/core'
 export default new Router().fallback(({ renderWithApp }) => {
   renderWithApp()
 })
+
+// In production be specific like follows to not overuse serverless functions for any request that shouldn't be entertained
+// import { Router } from '@layer0/core'
+
+// export default new Router()
+//   .get('/', ({ renderWithApp }) => {
+//     renderWithApp()
+//   })
+//   .post('/', ({ renderWithApp }) => {
+//     renderWithApp()
+//   })
